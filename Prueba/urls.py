7ca4fc8535ffbca26c5a1b76e2b44a654881997e
fguereta <fraanquito@gmail.com>
2015-08-31 23:27:57 -0300

    modificado 23:27

--- Prueba/urls.py
+++ Prueba/urls.py
@@ -1,5 +1,6 @@
 """Prueba URL Configuration
 fafasfa
+franco guereta
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
 Examples:
