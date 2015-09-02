6ecbca09f9d95de2b6146a478cf01d14c408e1bd
fguereta <fraanquito@gmail.com>
2015-09-02 17:58:24 -0300

    test

--- ingrese2numeros.py
+++ ingrese2numeros.py
@@ -1,9 +1,33 @@
 a=input("ingrese un numero ")
 b=input("ingrese otro numero ")
 print 'numeros ingresados ',a,b
-print 'programacion'
-print 'franco guereta'
-print 'hola'
-print 'aguante boca'
-print '7 libertadores'
-print'aguante el rojooo'
+op=1
+while(op!=0):
+    
+    print '1-sumar'
+    print '2-restar'
+    print '3-dividir'
+    print '4-multiplicar'
+    
+    op=input('ingrese la opcion que desea realizar: ')
+    
+    if(op==1):
+        res=(a+b)
+        print 'el resultado es: ',res
+    if(op==2):
+        res=(a-b)
+        print 'el resultado es: ',res
+    if(op==3):
+        res=(a/b)
+        print 'el resultado es: ',res
+    if(op==4):
+        res=(a*b)
+        print 'el resultado es: ',res
+        
+    print '1-sumar'
+    print '2-restar'
+    print '3-dividir'
+    print '4-multiplicar'
+    op=input('ingrese la opcion que desea realizar: ')    
+    
+
