1b9a10f4a868936aa7fd1a8eddc84f11edad5a8b
fguereta <fraanquito@gmail.com>
2015-09-04 02:31:48 -0300

    test

--- django/src/programa_django/urls.py
+++ django/src/programa_django/urls.py
@@ -1,5 +1,4 @@
 """programa_django URL Configuration
-
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
 Examples:
