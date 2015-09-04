a4d84041d0ad7ffab2cb0901c2a1a44bfc8f29e6
fguereta <fraanquito@gmail.com>
2015-09-03 21:19:38 -0300

    error resuelto

--- django/src/programa_django/settings.py
+++ django/src/programa_django/settings.py
@@ -87,8 +87,8 @@ DATABASES = {
         #'NAME': os.path.join(BASE_DIR, 'db.postgresql_psycopg2'),
         'NAME': 'tesis',
         'USER': 'postgres',
-        'PASSWORD': 'programacion.13',
-        'HOST':'192.168.1.38',
+        'PASSWORD': '123456',
+        'HOST':'127.0.0.1',
         'PORT':'5432',
         
         
